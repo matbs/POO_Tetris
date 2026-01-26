729f8a22d6cc8b8acad6aa82b6c14f4eb583ff33
Mateus <mateusbs20222@gmail.com>
2026-01-26 15:22:50 +0100

    telinha multiplayer online

--- states/StateTetrisOnline.cpp
+++ states/StateTetrisOnline.cpp
@@ -193,12 +193,140 @@ std::unique_ptr<IState> StateTetrisOnline::Update() {
     DrawText("YOU", 50, 50, 20, GREEN);
     DrawText("ENEMY", 400, 50, 20, RED);
 
-    if (localController.isGameOver()) {
-        DrawText("YOU LOST!", 100, 300, 40, RED);
+if (localController.isGameOver() || remoteController.isGameOver()) {
+    int localScore = localController.getScore();
+    int remoteScore = remoteController.getScore();
+    bool localWon = false;
+    bool remoteWon = false;
+    bool tie = false;
+    
+    if (localController.isGameOver() && remoteController.isGameOver()) {
+        if (localScore > remoteScore) {
+            localWon = true;
+        } else if (remoteScore > localScore) {
+            remoteWon = true;
+        } else {
+            tie = true;
+        }
+    } else if (localController.isGameOver()) {
+        remoteWon = true;
+    } else if (remoteController.isGameOver()) {
+        localWon = true;
     }
-    if (remoteController.isGameOver()) {
-        DrawText("YOU WIN!", 450, 300, 40, GREEN);
+
+    if(localController.isGameOver() && !remoteController.isGameOver()){
+        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), {0, 0, 0, 220});
+        int panelWidth = 400;
+        int panelHeight = 200;
+        int panelX = (GetScreenWidth() - panelWidth) / 2;
+        int panelY = (GetScreenHeight() - panelHeight) / 2;
+        
+        DrawRectangleLines(panelX, panelY, panelWidth, panelHeight, LIGHTGRAY);
+        DrawRectangleLines(panelX + 2, panelY + 2, panelWidth - 4, panelHeight - 4, DARKGRAY);
+        
+        // Centralizar o texto no retângulo
+        const char* text = "YOU LOST! WAITING FOR OPPONENT...";
+        int fontSize = 20;
+        int textWidth = MeasureText(text, fontSize);
+        int textX = panelX + (panelWidth - textWidth) / 2;
+        int textY = panelY + (panelHeight - fontSize) / 2; // Aproximadamente centralizado verticalmente
+        
+        DrawText(text, textX, textY, fontSize, RED);
     }
+    
+
+    if (localController.isGameOver() && remoteController.isGameOver())
+    {
+        DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), {0, 0, 0, 220});
+
+        int panelWidth = 500;
+        int panelHeight = 400;
+        int panelX = (GetScreenWidth() - panelWidth) / 2;
+        int panelY = (GetScreenHeight() - panelHeight) / 2;
+
+        DrawRectangleLines(panelX, panelY, panelWidth, panelHeight, LIGHTGRAY);
+        DrawRectangleLines(panelX + 2, panelY + 2, panelWidth - 4, panelHeight - 4, DARKGRAY);
+
+        int titleWidth = MeasureText("GAME OVER", 32);
+        DrawText("GAME OVER", panelX + (panelWidth - titleWidth) / 2, panelY + 20, 32, MAGENTA);
+
+        DrawRectangle(panelX + 30, panelY + 65, panelWidth - 60, 1, {100, 100, 100, 150});
+
+        std::string resultText;
+        Color resultColor;
+        
+        if (tie) {
+            resultText = "IT'S A TIE!";
+            resultColor = YELLOW;
+        } else if (localWon) {
+            resultText = "VICTORY!";
+            resultColor = GREEN;
+        } else {
+            resultText = "DEFEAT!";
+            resultColor = RED;
+        }
+
+        int resultWidth = MeasureText(resultText.c_str(), 36);
+        DrawText(resultText.c_str(), panelX + (panelWidth - resultWidth) / 2, panelY + 90, 36, resultColor);
+
+        std::string scoreMessage;
+        if (tie) {
+            scoreMessage = TextFormat("Both players scored %d points!", localScore);
+        } else if (localWon) {
+            scoreMessage = TextFormat("You won by %d points!", localScore - remoteScore);
+        } else {
+            scoreMessage = TextFormat("You lost by %d points!", remoteScore - localScore);
+        }
+        
+        int messageWidth = MeasureText(scoreMessage.c_str(), 20);
+        DrawText(scoreMessage.c_str(), panelX + (panelWidth - messageWidth) / 2, panelY + 140, 20, LIGHTGRAY);
+
+        DrawRectangle(panelX + 50, panelY + 170, panelWidth - 100, 1, {80, 80, 80, 100});
+
+        int statsY = panelY + 190;
+
+        DrawText("SCOREBOARD", panelX + (panelWidth - MeasureText("SCOREBOARD", 24)) / 2, statsY, 24, LIGHTGRAY);
+
+        int scoresY = statsY + 40;
+
+        Color localScoreColor = localWon ? GREEN : (tie ? YELLOW : LIGHTGRAY);
+        Color remoteScoreColor = remoteWon ? GREEN : (tie ? YELLOW : LIGHTGRAY);
+
+        int scorePanelWidth = 200;
+        int leftScoreX = panelX + (panelWidth/2 - scorePanelWidth - 20);
+        int rightScoreX = panelX + (panelWidth/2 + 20);
+
+        DrawText("YOUR SCORE", leftScoreX, scoresY, 18, LIGHTGRAY);
+        DrawText(TextFormat("%d", localScore), 
+                leftScoreX + (scorePanelWidth - MeasureText(TextFormat("%d", localScore), 28)) / 2, 
+                scoresY + 30, 28, localScoreColor);
+
+        DrawText("OPPONENT SCORE", rightScoreX, scoresY, 18, LIGHTGRAY);
+        DrawText(TextFormat("%d", remoteScore), 
+                rightScoreX + (scorePanelWidth - MeasureText(TextFormat("%d", remoteScore), 28)) / 2, 
+                scoresY + 30, 28, remoteScoreColor);
+
+        DrawRectangle(panelX + panelWidth/2 - 1, scoresY - 10, 2, 80, {100, 100, 100, 150});
+
+        int linesY = scoresY + 70;
+        DrawText(TextFormat("Lines: %d", localController.getLinesCleared()), leftScoreX, linesY, 18, {200, 200, 200, 200});
+        DrawText(TextFormat("Lines: %d", remoteController.getLinesCleared()), rightScoreX, linesY, 18, {200, 200, 200, 200});
+
+        DrawRectangle(panelX + 40, panelY + panelHeight - 70, panelWidth - 80, 1, {60, 60, 60, 100});
+
+        int instructionsY = panelY + panelHeight - 40;
+
+        int restartWidth = MeasureText("R - Restart Match", 20);
+        int menuWidth = MeasureText("ESC - Main Menu", 20);
+        int totalWidth = restartWidth + 30 + menuWidth;
+        
+        DrawText("ESC - Exit Game", 
+                panelX + (panelWidth - totalWidth) / 2 + 120, 
+                instructionsY, 
+                20, 
+                LIGHTGRAY);
+    }
+}
 
     Color btnColor = isMouseOver ? LIGHTGRAY : GRAY;
     Color textColor = isMouseOver ? BLACK : WHITE;
